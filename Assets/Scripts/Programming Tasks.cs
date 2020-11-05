ec649c6cf94f1fa111ca4c4525528cae054f968e
Lala-banners <lala.l.banno@hotmail.com>
2020-11-05 16:13:47 +1100

    City Core has health, set up for future

---
+++ Assets/Scripts/Programming Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgrammingTasks : MonoBehaviour
+{
+    /* Lara Tasks
+     * Tower creation + turret inventory for player to drag into game
+     * Turret spawner
+     * Buy turret system + enemy killed gives money
+     * Turrets shoot and destroy enemies
+     * Tower/Base health
+     * 
+     */
+
+    /* Cleon Tasks
+     * Main menu, options menu, in-game GUI
+     * Enemy AI
+     * Enemy spawning
+     * Money system UI
+     * Enemy health
+     * 
+     */
+}
---
+++ Assets/Scripts/Tower/TowerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerHealth : MonoBehaviour
+{
+    #region Variables
+    private float maxHealth = 100;
+    public Slider healthSlider;
+    private float currentHealth;
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10f);
+        }
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        currentHealth = currentHealth - Damage;
+        healthSlider.value = currentHealth;
+    }
+
+}

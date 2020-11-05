6cba16e441ad4edab617fceb778b1cfaa51e4f81
Lala-banners <lala.l.banno@hotmail.com>
2020-11-05 16:43:04 +1100

    Updated programming tasks, making turret drag and drop

--- Assets/Scripts/Programming Tasks.cs
+++ Assets/Scripts/Programming Tasks.cs
@@ -10,6 +10,12 @@ public class ProgrammingTasks : MonoBehaviour
      * Buy turret system + enemy killed gives money
      * Turrets shoot and destroy enemies
      * Tower/Base health
+     * Drag and drop turrets steps:
+     * 1. Drag UI (detect when ui is being dragged)
+     * 2. Spawn a turret model that stays on the mouse position
+     * 3. Create turret where you let go of mouse
+     * 
+     * 
      * 
      */
 

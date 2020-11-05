c58ca8f3892862d269914f6f5ff4830708017635
Lala-banners <lala.l.banno@hotmail.com>
2020-11-05 18:19:54 +1100

    getting to spawning fake turret from inventory - not yet though

---
+++ Assets/Scripts/Tower/SpawnTurretOnClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnTurretOnClick : MonoBehaviour
+{
+    Ray ray; //create the ray
+    RaycastHit hit; //creating the raycast hit
+    public GameObject turretToInstantiate; //the object that will be instantiated is the turret
+    public Image[] turretSlots;
+    //Spawn location set to turret slot
+    //Instantiate on click FROM INVENTORY TURRET SLOTS to the grid
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Tells ray it will spawn from the center of the camera to the mouse position
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            //If mouse button left is down
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Creates a prefab wherever user clicks
+                Instantiate(turretToInstantiate, hit.point, Quaternion.identity);
+                //turretToInstantiate.Instantiate<Transform>(hit.point, turretSlots, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TurretDropHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TurretDropHandler : MonoBehaviour, IDropHandler
+{
+    public GameObject inventory;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+
+        print("OnDrop");
+    }
+
+}

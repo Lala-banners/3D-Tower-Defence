f46a39137d7318bdc67c0288acdfbcd324b9af93
Lala-banners <lala.l.banno@hotmail.com>
2020-11-26 09:45:07 +1100

    Camera changes

--- Assets/OrbitCam.cs
+++ Assets/OrbitCam.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class OrbitCam : MonoBehaviour
-    {​​​​
-        public Camera attachedCamera;
-        public float minYAngle = 30f, maxYAngle = 90f;
-        public float ySpeed = 120f, xSpeed = 120f;
-        void FixedUpdate()
-        {​​​​
-            if (Input.GetMouseButton(1))
-            {​​​​
-                float mouseX = Input.GetAxis("Mouse X");
-                float mouseY = Input.GetAxis("Mouse Y");
-                Vector3 euler = transform.eulerAngles;
-                euler.x -= mouseY * ySpeed * Time.deltaTime;
-                euler.y += mouseX * xSpeed * Time.deltaTime;
-                euler.x = Mathf.Clamp(euler.x, minYAngle, maxYAngle);
-                transform.eulerAngles = euler;
-            }​​​​
-        }​​​​
-    }​​​​
+public class OrbitCam : MonoBehaviour
+{
+    public Camera attachedCam;
 
+    public float minYAngle = 30f, maxYAngle = 90f;
+    public float ySpeed = 120f, xSpeed = 120f;
 
-
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+    private void FixedUpdate()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            Vector3 euler = transform.eulerAngles;
+            euler.x -= mouseY * ySpeed * Time.deltaTime;
+            euler.y += mouseX * xSpeed * Time.deltaTime;
+            euler.x = Mathf.Clamp(euler.x, minYAngle, maxYAngle);
+            transform.eulerAngles = euler;
+        }
+    }
+}
